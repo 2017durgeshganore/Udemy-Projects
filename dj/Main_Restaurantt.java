392da5da3428027c54cac336890640b8ca67f522
2017durgeshganore <90857992+2017durgeshganore@users.noreply.github.com>
2023-01-19 14:26:48 +0000

    Add files via upload

---
+++ dj/Main_Restaurantt.java
@@ -0,0 +1,16 @@
+package marocrestaurant;
+
+public class Main_Restaurantt {
+	
+	protected String first_mealcourse = "Moroccan Nicoise Salad";
+	protected String main_mealcourse = "Meat Plum Tagine";
+	protected String dessert = "Briouat";
+	protected String tea = "Moroccan Mint Tea";
+	
+	protected double first_mealcourseprice = 150;
+	protected double main_mealcourseprice = 220.50;
+	protected double dessert_price = 80.50;
+	protected double tea_price = 60;
+	
+
+}
